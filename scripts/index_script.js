8366420817df1640fc60728f4e5bf2bcd08f3341
gal1260 <gantar.gal@gmail.com>
2019-02-03 21:54:23 +0100

    Izboljšal index.html

--- scripts/index_script.js
+++ scripts/index_script.js
@@ -1,18 +1,66 @@
-function insertRandomImage() {
-  let images = {"NASLOVNA_RANDOM.jpg":"white", "pexels-photo-266691.jpeg":"white", "pexels-photo-414171.jpeg":"white"}; //seznam vseh slik (in barv teksta na njih), ki se bodo naključno prikazovale na naslovni strani
-  let keys = Object.keys(images);
+function getTheValues() {
+  imageLocation = document.body.querySelector("#main-image");
+  textLocation = document.body.querySelector("#links");
+  logoLocation = document.body.querySelector("#logo-image");
 
-  index = Math.floor(Math.random()*keys.length)
-  randomPhoto = keys[index];
-  textColor = images[keys[index]];
-
-  imageLocation = document.querySelector("#main-image");
-  imageLocation.setAttribute("src", "images/index/" + randomPhoto);
-
-  textLocationsList = document.querySelectorAll(".links, a");
-  textLocationsList.forEach(element => {
-    element.style.color = textColor;
-    element.style.fontFamily = "Verdana";
-    }
-  );
+  landscapeImages = {"pexels-photo-266691.jpeg":"white", "pexels-photo-414171.jpeg":"white"}; //seznam vseh slik (in barv teksta na njih), ki se bodo naključno prikazovale na naslovni strani
+  landscapeKeys = Object.keys(landscapeImages);
+  portraitImages = {"portrait.jpg":"white"};
+  portraitKeys = Object.keys(portraitImages);
 }
+function insertRandomImage(isMobile) {
+  if (isMobile) {
+    index = Math.floor(Math.random()*portraitKeys.length)
+    randomPhotoLocation = "portrait/" + portraitKeys[index];
+    textColor = portraitImages[portraitKeys[index]];
+  }
+  else {
+    index = Math.floor(Math.random()*landscapeKeys.length)
+    randomPhotoLocation = "landscape/" + landscapeKeys[index];
+    textColor = landscapeImages[landscapeKeys[index]];
+  }
+
+  imageLocation.setAttribute("src", "images/index/" + randomPhotoLocation);
+  logoLocation.setAttribute("src", "images/index/logo/" + textColor + ".png");
+  textLocation.style.color = textColor;
+}
+function alignTheText() {
+  screenWidth = screen.width;
+  if (screenWidth < 576) {
+    textLocation.style.left = "50%";
+    textLocation.style.transform = "translateX(-50%)";
+    textLocation.style.fontSize = "400%";
+  }
+  else {
+    textLocation.style.left = null;
+    textLocation.style.transform = null;
+    textLocation.style.fontSize = null;
+  }
+}
+
+setInterval (function() {
+  if (typeof wasMobile === 'undefined') {
+    if (screen.width < 576) {
+      insertRandomImage(true);
+    }
+    else {
+      insertRandomImage(false);
+    }
+  }
+  else if (wasMobile == (screen.width < 576)) {}
+  else if (wasMobile || !(screen.width < 576)) {
+    insertRandomImage(false);
+  }
+  else if (!wasMobile || (screen.width < 576)) {
+    insertRandomImage(true);
+  }
+
+  if (screen.width < 576) {
+    wasMobile = true;
+  }
+  else {
+    wasMobile = false;
+  }
+
+  alignTheText();
+}, 10)
