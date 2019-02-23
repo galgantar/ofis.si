1a1ce45a1d2c1158d29fc68de5873a008238bca5
gal1260 <gantar.gal@gmail.com>
2019-02-23 20:21:45 +0100

    Popravki, popravki, popravki

--- scripts/project_scripts.js
+++ scripts/project_scripts.js
@@ -3,11 +3,14 @@ mapboxMapGlobal.classList.add("map-row-hidden");
 
 function hideMap() {
   mapboxMap = document.querySelector(".map-row");
+  button = document.querySelector(".show-map-button");
 
   if (mapboxMap.classList.contains("map-row-hidden")) {
     mapboxMap.classList.remove("map-row-hidden");
+    button.style.transform = "rotate(180deg)";
   }
   else {
     mapboxMap.classList.add("map-row-hidden");
+    button.style.transform = "none";
   }
 }
