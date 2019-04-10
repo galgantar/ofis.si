abe84205d20d08401bb7c23f989ced93339d41cf
gal1260 <gantar.gal@gmail.com>
2019-04-11 01:24:57 +0200

    Pofilal press, pomembnejše novice, manjkajoče slovenske podstrani, naslovno stran

---
+++ scripts/news_read_more.js
@@ -0,0 +1,32 @@
+function show_news() {
+  if (sections[1].style.display == "" || sections[1].style.display == "none") {
+    sections.forEach(function(element){
+      element.style.display = "block";
+    });
+    archive.style.display = "inline-block";
+
+    if (lang == "en") {
+      button.innerHTML = "Show less";
+    }else {
+      button.innerHTML = "Pokaži manj";
+    }
+  }
+  else {
+    sections.forEach(function(element){
+      element.style.display = "none";
+    });
+    archive.style.display = "none";
+
+    if (lang == "en") {
+      button.innerHTML = "Show more";
+    }else {
+      button.innerHTML = "Pokaži več";
+    }
+  }
+
+}
+
+sections = Array.prototype.slice.call(document.querySelectorAll(".news-section")).slice(1);
+archive = document.querySelector("#show-more");
+button = document.querySelector(".show-news");
+lang = document.querySelector("html").getAttribute("lang");
